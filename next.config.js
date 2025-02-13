f582badb6b7171e5de859ebce5fa486a842dc22c
Hackerug06 <hackerug06@gmail.com>
2025-02-13 03:58:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals.push("tailwindcss-animate")
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
 
-      
