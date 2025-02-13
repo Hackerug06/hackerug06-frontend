82de89dcac236c34fea199baa25313f619f0cae7
Hackerug06 <hackerug06@gmail.com>
2025-02-13 04:20:58 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["placeholder.com"], // Add any domains you're loading images from
+  },
 }
 
 module.exports = nextConfig
