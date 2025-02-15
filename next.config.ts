7d21d74dcc443a4878c919eeb34620a4ffcb3123
Hackerug06 <hackerug06@gmail.com>
2025-02-15 10:34:07 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["placeholder.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+module.exports = nextConfig
 
-export default nextConfig;
+    
